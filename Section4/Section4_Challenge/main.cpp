c0e260b9645bd6f43fa25d121d9ce94f872b4e4d
Andy Lang <djdreviz@gmail.com>
2023-06-29 01:04:54 +0300

    34. Section Challenge done

---
+++ Section4/Section4_Challenge/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int favNumber;
+    cout << "Enter your favorite number between 1 and 100: ";
+    cin >> favNumber;
+    
+    cout << "Amazing!! That's my favorite number too!" << endl;
+    cout << "No really!!, " << favNumber << " is my favorite number!" << endl;
+    
+    return 0;
+}
