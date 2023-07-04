5c6ca383e2643fc43e538c68e3d790c21efb3962
Andy Lang <djdreviz@gmail.com>
2023-07-05 01:54:00 +0300

    Coding Exercise 5: Declaring, Initializing and Accessing an Array done!

---
+++ Section7/CodingExercise5/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int arr[10] {0};
+    
+    arr[0] = 100;
+    arr[9] = 1000;
+    
+    cout << arr[0] << " and " << arr[9] << endl;
+    
+    
+    return 0;
+}
+
+// Exercise done!
