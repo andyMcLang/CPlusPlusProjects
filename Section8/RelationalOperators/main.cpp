d7525a44c0c7169efaec32b90e518cc5032ff028
Andy Lang <djdreviz@gmail.com>
2023-07-09 02:17:42 +0300

    70. Relational Operators done!

---
+++ Section8/RelationalOperators/main.cpp
@@ -0,0 +1,21 @@
+// 70. Relational Operators
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1{}, num2{};
+    
+    cout << boolalpha;
+    cout << "Enter 2 integers separated by a space: ";
+    cin >> num1 >> num2;
+    
+    cout << num1 << " > " << num2 << " : " << (num1 > num2) << endl;
+    cout << num1 << " >= " << num2 << " : " << (num1 >= num2) << endl;
+    cout << num1 << " < " << num2 << " : " << (num1 < num2) << endl;
+    cout << num1 << " <= " << num2 << " : " << (num1 <= num2) << endl;   
+
+    return 0;
+}
